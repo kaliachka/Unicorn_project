ac858c54b468e8c89044bfe2d17e20238176a1fd
Ksenia Kaliachka <59210216+kaliachka@users.noreply.github.com>
2021-02-05 16:43:05 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.slider').slick( {
+        infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  nextArrow: "<img src='./media/pointer.png'>",
+  prevArrow: false
+    });
+});
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
